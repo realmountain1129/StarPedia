7719a4579128b029b0ccf2746bde1affb55da7b5
Jinsan Kim <gkjs1129@gmail.com>
2022-05-06 01:27:07 +0900

    Initial Commit

---
+++ StarPedia/StarPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarPediaApp.swift
+//  StarPedia
+//
+//  Created by Jinsan Kim on 2022/05/06.
+//
+
+import SwiftUI
+
+@main
+struct StarPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
